19ba0be329fe9c1f946ce29d337ee27039ca4b24
unknown <adhamelgohary@aucegypt.edu>
2023-11-02 22:23:45 +0200

    version 1

---
+++ android/app/src/main/java/com/example/ma7fazti/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.ma7fazti;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/signup.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:ma7fazti/signin.dart';
+
+class SignUpPage extends StatefulWidget {
+  @override
+  _SignUpPageState createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+  String _message = '';
+
+  Future<void> _signUp() async {
+    try {
+      UserCredential userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: _emailController.text,
+        password: _passwordController.text,
+      );
+
+      // Additional logic to store the user's name in Firestore or Realtime Database
+      // You can use userCredential.user.uid as the user's unique identifier
+
+      // Navigate to the home page or any other page after successful sign up
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => SignInPage()),
+      );
+    } catch (e) {
+      print('Sign up failed: $e');
+
+      // Check the error type and access the code property
+      if (e is FirebaseAuthException) {
+        if (e.code == 'weak-password') {
+          setState(() {
+            _message = 'The password provided is too weak.';
+          });
+        } else if (e.code == 'email-already-in-use') {
+          setState(() {
+            _message = 'The account already exists for that email.';
+          });
+        } else if (e.code == 'invalid-email') {
+          setState(() {
+            _message = 'Please enter a valid email address.';
+          });
+        } else {
+          setState(() {
+            _message = 'An error occurred during sign up. Please try again later.';
+          });
+        }
+      } else {
+        setState(() {
+          _message = 'An error occurred during sign up. Please try again later.';
+        });
+      }
+    }
+  }
+
+
+  void _goToSignInPage() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => SignInPage()),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sign Up'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Welcome to Ma7fazti!',
+              style: TextStyle(
+                fontSize: 24.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 16.0),
+            TextField(
+              controller: _emailController,
+              decoration: InputDecoration(
+                labelText: 'Email',
+              ),
+            ),
+            SizedBox(height: 16.0),
+            TextField(
+              controller: _passwordController,
+              decoration: InputDecoration(
+                labelText: 'Password',
+              ),
+              obscureText: true,
+            ),
+            SizedBox(height: 16.0),
+            ElevatedButton(
+              child: Text('Sign Up'),
+              onPressed: _signUp,
+            ),
+            SizedBox(height: 16.0),
+            ElevatedButton(
+              child: Text('Already have an account? Sign In'),
+              onPressed: _goToSignInPage,
+            ),
+            SizedBox(height: 18.0),
+            Text(
+              _message,
+              style: TextStyle(
+                color: Colors.red,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
