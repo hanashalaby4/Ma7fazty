9ce50514ffb0c112eb1a0fd5d690754a26b7eaad
hanashalaby4 <shalaby04@aucegypt.edu>
2023-11-03 15:30:53 +0200

    Added CustomText and CustomTextField classes to custom_widgets.dart

--- custom_widgets.dart
+++ custom_widgets.dart
@@ -14,5 +14,65 @@ class CustomButton extends StatelessWidget {
 }) : super(key: key); //passing key to stateless widget constructor
 
   @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+        onPressed: onPressed,
+        style: ElevatedButton.styleFrom(
+          backgroundColor: color,
+        ),
+      child: Text(text),
 
+    );
+  }
+}
+
+class CustomTextField extends StatelessWidget {
+  final TextEditingController controller;
+  final String labelText;
+  final bool obscureText;
+
+  const CustomTextField ({
+    Key? key,
+    required this.controller,
+    required this.labelText,
+    this.obscureText = false,
+}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: controller,
+      decoration: InputDecoration(
+        labelText: labelText,
+      ),
+      obscureText: obscureText,
+    );
+  }
+}
+
+class CustomText extends StatelessWidget {
+  final String text;
+  final double fontSize;
+  final FontWeight fontWeight;
+  final Color color;
+
+  const CustomText({
+    Key? key,
+    required this.text,
+    this.fontSize = 16.0,
+    this.fontWeight = FontWeight.normal,
+    this.color = Colors.black,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: TextStyle(
+        fontSize: fontSize,
+        fontWeight: fontWeight,
+        color: color,
+      ),
+    );
+  }
 }
