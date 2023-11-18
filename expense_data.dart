eb3e1815d94df8cd8a3cacf41587ff1c51c8337e
hanashalaby4 <shalaby04@aucegypt.edu>
2023-11-18 17:57:40 +0200

    added expense data class

---
+++ expense_data.dart
@@ -0,0 +1,7 @@
+class ExpenseData {
+  final DateTime date;
+  final double amount;
+  final String category;  // Assuming you want to include category
+
+  ExpenseData(this.date, this.amount, this.category);
+}
