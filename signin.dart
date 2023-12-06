936ba6cfd3ddc8a33b82b272604f9e25682b78f1
hanashalaby4 <124688687+hanashalaby4@users.noreply.github.com>
2023-12-06 10:40:13 +0200

    Update signin.dart

--- signin.dart
+++ signin.dart
@@ -1,97 +1,36 @@
-import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:ma7fazti/home.dart'
-import 'custom_widgets.dart';
+import 'package:firebase_core/firebase_core.dart'; //Import statement to import the firebase_core package that initialized firbase in the app
+import 'package:flutter/material.dart'; //import material package (design widgets)
+import 'signup.dart';
+import 'color.dart';
+import 'firebase_options.dart'; //import a dart file that contains the details needed to establish a connection between Ma7fazti and firebase services
 
-class SignInPage extends StatefulWidget {
-  @override
-  _SignInPageState createState() => _SignInPageState();
+
+void main() async { //entry point of the app
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
 }
 
-class _SignInPageState extends State<SignInPage> {
-  TextEditingController _emailController = TextEditingController();
-  TextEditingController _passwordController = TextEditingController();
-  String _message = '';
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-  Future<void> _signIn() async {
-    try {
-      UserCredential userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(
-        email: _emailController.text,
-        password: _passwordController.text,
-      );
-
-      // Navigate to the home page with user credentials
-      Navigator.push(
-        context,
-        MaterialPageRoute(
-          builder: (context) => HomePage(userCredential), // Pass the user credentials here
-        ),
-      );
-    } catch (e) {
-      print('Sign in failed: $e');
-
-      // Check the error type and access the code property
-      if (e is FirebaseAuthException) {
-        if (e.code == 'user-not-found') {
-          setState(() {
-            _message = 'No user found for that email.';
-          });
-        } else if (e.code == 'wrong-password') {
-          setState(() {
-            _message = 'Incorrect password.';
-          });
-        } else if (e.code == 'invalid-email') {
-          setState(() {
-            _message = 'Please enter a valid email address.';
-          });
-        } else {
-          setState(() {
-            _message = 'An error occurred during sign in. Please try again later.';
-          });
-        }
-      } else {
-        setState(() {
-          _message = 'An error occurred during sign in. Please try again later.';
-        });
-      }
-    }
-  }
-
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Sign In'),
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            CustomTextField(
-              controller: _emailController,
-              labelText: 'Email',
-            ),
-            SizedBox(height: 16.0),
-            CustomTextField(
-              controller: _passwordController,
-              labelText: 'Password',
-              obscureText: true,
-            ),
-            SizedBox(height: 16.0),
-            CustomButton(
-               text: 'Sign In',
-              onPressed: _signIn,
-            ),
-            SizedBox(height: 16.0),
-            CustomText(
-                text: _message,
-                color: Colors.red,
-                fontWeight: FontWeight.bold,
-                )
-          ],
+    return MaterialApp(
+      title: 'Ma7fazti',
+      theme: ThemeData(
+        textTheme:
+        TextTheme(titleMedium: TextStyle(color: softPurple),
         ),
+      scaffoldBackgroundColor: const Color(0xff212131),
+        primaryColor: const Color(0xffCBCCD5),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
+      home: SignUpPage(), //sets up the initial screen for Ma7fazti
     );
   }
 }
