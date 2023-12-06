54d5b7c75090faa7f5c6d9381010bc167133756c
hanashalaby4 <124688687+hanashalaby4@users.noreply.github.com>
2023-12-06 10:38:12 +0200

    Delete expense_data.dart

--- expense_data.dart
+++
@@ -1,7 +0,0 @@
-class ExpenseData {
-  final DateTime date;
-  final double amount;
-  final String category;  // Assuming you want to include category
-
-  ExpenseData(this.date, this.amount, this.category);
-}
