e170e2956576e77eb2d3eedff251d066db24b265
hanashalaby4 <124688687+hanashalaby4@users.noreply.github.com>
2023-12-06 10:37:21 +0200

    Create color.dart

---
+++ color.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+// theme colors
+    Color blackPrimary = Color(0xff212131);
+    Color black = Color(0xff09050D);
+    Color grey = Color(0xffc8c3e8);
+    Color white = Color(0xffffffff);
+    Color purple = Color(0xff6d48ff);
+
+    Color pink = Color(0xfff85c96);
+    Color cyan = Color (0xff63adbd);
+    Color orange = Color(0xffff9e52);
+
+    Color softPurple = Color(0xffD0C3FF);
+    Color softPink = Color(0xfffda1c1);
+    Color softOrange = Color(0xfffdba87);
+    Color softCyan = Color(0xff63adbd);
+    Color softGreen = Color(0xff90fabc);
+    Color softYellow = Color(0xffeffa90);
+
+    List<Color> customColors = [
+        softPurple,
+        softCyan,
+        softOrange,
+        softPink,
+        softGreen,
+        softYellow,
+    ];
